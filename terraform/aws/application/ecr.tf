8143a716107b52634883cbf0fea5c6c43588190a
Petri Toivari <petri.toivari@nitor.com>
2026-07-27 22:19:45 +0300

    Add manual Destroy workflow to tear down the application stack; force_delete ECR so destroy can drop the non-empty repo

--- terraform/aws/application/ecr.tf
+++ terraform/aws/application/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "app" {
   name                 = var.ecr_repository_name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true # allow `terraform destroy` to remove the repo without emptying it first
 
   image_scanning_configuration {
     scan_on_push = true
